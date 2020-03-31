a9647816543a8f3fcd011e6da4db7121c942cbce
Brian Kahn <brian.kahn@noaa.gov>
2020-03-31 15:25:00 -0700

    updated maketar with kahn usrlocal servers

--- maketar.sh
+++ maketar.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-New=~/Linux
-Kahn=".bash* .dir_colors .minttyrc .gitconfig .k .ssh/authorized_keys .vim"
+Lnx=~/Linux
+Kahn=".bash* .dir_colors .minttyrc .gitconfig .k .ssh/authorized_keys .vim bin"
 cd
-echo "rm $New/tar/*.tar"
-rm $New/tar/*.tar
-echo "tar cf $New/tar/kahn.tar $Kahn"
-tar cf $New/tar/kahn.tar $Kahn
-echo "tar cf $New/tar/notes.tar notes"
-tar cf $New/tar/notes.tar notes
-echo "cd $New/opt; tar cf $New/tar/opt.tar ."
-cd $New/opt; tar cf $New/tar/opt.tar .
-echo "cd $New/etc; tar cf $New/tar/etc.tar ."
-cd $New/etc; tar cf $New/tar/etc.tar .
+echo "rm $Lnx/tar/*.tar"
+rm $Lnx/tar/*.tar
+echo "tar cf $Lnx/tar/kahn.tar $Kahn"
+tar cf $Lnx/tar/kahn.tar $Kahn
+echo "tar cf $Lnx/tar/notes.tar notes"
+tar cf $Lnx/tar/notes.tar notes
+echo "cd $Lnx/opt; tar cf $Lnx/tar/opt.tar ."
+cd $Lnx/opt; tar cf $Lnx/tar/opt.tar .
+echo "cd $Lnx/etc; tar cf $Lnx/tar/etc.tar ."
+cd $Lnx/etc; tar cf $Lnx/tar/etc.tar .
+echo "cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar ."
+cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar .
+echo "cd $Lnx/servers; tar cf $Lnx/tar/servers.tar ."
+cd $Lnx/servers; tar cf $Lnx/tar/servers.tar .
