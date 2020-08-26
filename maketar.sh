21359b1a1c53fd141fdbd3640a6162afc6578061
Brian Kahn <brian.kahn@noaa.gov>
2020-08-26 16:01:01 -0700

    maketar: Notes

--- maketar.sh
+++ maketar.sh
@@ -4,10 +4,10 @@ Kahn=".bash* .dir_colors .minttyrc .gitconfig .k .ssh/authorized_keys .vim bin"
 cd
 echo "rm $Lnx/tar/*.tar"
 rm $Lnx/tar/*.tar
-echo "tar cf $Lnx/tar/kahn.tar $Kahn"
-tar cf $Lnx/tar/kahn.tar $Kahn
-echo "tar cf $Lnx/tar/notes.tar notes"
-tar cf $Lnx/tar/notes.tar notes
+echo "cd ~; tar cf $Lnx/tar/kahn.tar $Kahn"
+cd ~; tar cf $Lnx/tar/kahn.tar $Kahn
+echo "cd ~; tar cf $Lnx/tar/notes.tar Notes"
+cd ~; tar cf $Lnx/tar/notes.tar Notes
 echo "cd $Lnx/opt; tar cf $Lnx/tar/opt.tar ."
 cd $Lnx/opt; tar cf $Lnx/tar/opt.tar .
 echo "cd $Lnx/etc; tar cf $Lnx/tar/etc.tar ."
@@ -16,3 +16,5 @@ echo "cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar ."
 cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar .
 echo "cd $Lnx/servers; tar cf $Lnx/tar/servers.tar ."
 cd $Lnx/servers; tar cf $Lnx/tar/servers.tar .
+#
+echo "git clone git@github.com:brikahnoaa/Linux.git"
