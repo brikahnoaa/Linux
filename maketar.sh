50c9c370167fba2e86beb73217bd0c531801e2fe
Brian Kahn <brian.kahn@noaa.gov>
2020-10-02 16:38:26 -0700

    maketar cron/yum-update

--- maketar.sh
+++ maketar.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 Lnx=~/Linux
-Kahn=".bash* .dir_colors .minttyrc .gitconfig .k .ssh/authorized_keys .vim bin"
-cd
-echo "rm $Lnx/tar/*.tar"
-rm $Lnx/tar/*.tar
-echo "cd ~; tar cf $Lnx/tar/kahn.tar $Kahn"
-cd ~; tar cf $Lnx/tar/kahn.tar $Kahn
-echo "cd ~; tar cf $Lnx/tar/notes.tar Notes"
-cd ~; tar cf $Lnx/tar/notes.tar Notes
-echo "cd $Lnx/opt; tar cf $Lnx/tar/opt.tar ."
-cd $Lnx/opt; tar cf $Lnx/tar/opt.tar .
-echo "cd $Lnx/etc; tar cf $Lnx/tar/etc.tar ."
-cd $Lnx/etc; tar cf $Lnx/tar/etc.tar .
-echo "cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar ."
-cd $Lnx/usr/local; tar cf $Lnx/tar/usrlocal.tar .
-echo "cd $Lnx/servers; tar cf $Lnx/tar/servers.tar ."
-cd $Lnx/servers; tar cf $Lnx/tar/servers.tar .
+echo "cd $Lnx"
+cd $Lnx
+mkdir -p tar
+echo "rm -f tar/*.tar"
+rm -f tar/*.tar
+for i in opt etc usrlocal servers; do
+  echo "tar cf tar/$i.tar $i"
+  tar cf tar/$i.tar $i
+done
 #
 echo "git clone git@github.com:brikahnoaa/Linux.git"
