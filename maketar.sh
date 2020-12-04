c1611bb718746b1b17d0ed23b7282afa75c33888
Brian Kahn <brian.kahn@noaa.gov>
2020-12-04 15:25:38 -0800

    dots.tar bin.tar

--- maketar.sh
+++ maketar.sh
@@ -9,5 +9,9 @@ for i in opt etc usr servers; do
   echo "tar cf tar/$i.tar $i"
   tar cf tar/$i.tar $i
 done
+cd ~
+dots=.bash* .dir_colors .minttyrc .gitconfig .k .ssh/authorized_keys .vim/
+tar cf $Lnx/tar/dots.tar $dots
+tar cf $Lnx/tar/bin.tar bin
 #
 echo "git clone git@github.com:brikahnoaa/Linux.git"
